82db4aec16273118170a598cd7d65579bda3b7ae
Miguel Piedrafita <github@miguelpiedrafita.com>
2016-12-06 13:10:18 +0100

    Delete danger.rb

--- .danger/danger.rb
+++
@@ -1,8 +0,0 @@
-require 'rubygems'
-require 'json'
-require 'octokit'
-client = Octokit::Client.new(:access_token => GITHUB_DANGER_API_TOKEN)
-user = client.user
-user.login
-data = JSON.parse(json)
-Octokit.add_labels_to_an_issue("bayandin/awesome-awesomeness", data['number'], ['Changes Required'])
